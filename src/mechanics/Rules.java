7c1a274c13b25f4032259e47227d70952f1f2189
Tomasz Artur <Tomasz Artur>
2016-04-03 23:19:42 +0200

    Clearing code view

--- src/mechanics/Rules.java
+++ src/mechanics/Rules.java
@@ -10,12 +10,12 @@ public interface Rules {
 		int[] result;
 			if(squareID==0)
 				{int[] temp= {sizeOfBoard/2};result=temp;}//pierwsze pole A1 ma 1 po³¹czenie
-			else if(squareID==sizeOfBoard*sizeOfBoard/2-1) // ostatnie pole H8 ma 1 po³¹czenie
-				{int[] temp= {sizeOfBoard*sizeOfBoard/2-sizeOfBoard-1};result=temp;}
+			else if(squareID==board.length-1) // ostatnie pole H8 ma 1 po³¹czenie
+				{int[] temp= {squareID-sizeOfBoard-1};result=temp;}
 			
 			else if(squareID<sizeOfBoard/2) // pola z rzêdu 1(inne ni¿ A1) maj¹ 2 po³¹czenia
 				{int[] temp= {squareID+sizeOfBoard/2-1,squareID+sizeOfBoard/2};result=temp;}
-			else if(squareID>sizeOfBoard*sizeOfBoard/2-sizeOfBoard/2-1)// pola z rzêdu 8 maj¹ 2 
+			else if(squareID>board.length-sizeOfBoard/2-1)// pola z rzêdu 8 maj¹ 2 
 				{int[] temp= {squareID-sizeOfBoard/2,squareID-sizeOfBoard/2+1};result=temp;}
 			
 			else if((squareID%(sizeOfBoard/2)==0&&((squareID/(sizeOfBoard/2))%2)==0)//pola z kolumn A(parzyste rzêdy) i H (nieparzyste rzêdy) maj¹ 2
