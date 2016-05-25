375cc0ede67cf6aff6d89bb7c8b13b966e5e0ad2
Bartosz Sobczak <bartoszsobczak95@gmail.com>
2016-05-25 21:28:32 +0200

    Piece moves only in one direction but can capture in both

--- src/Rules.java
+++ src/Rules.java
@@ -6,34 +6,48 @@ public abstract class Rules {
 		boolean isAvalible = false;
 		if(board != null && square != null && piece != null && piece_square != null && square.getPiece() == null){ //all arguments are not null and square is empty
 			if((piece.getOwner() == OWNER.WHITE && board.getTurn() == true) || piece.getOwner() == OWNER.BLACK && board.getTurn() == false){ //turn condition
-				if(connectedTo(board, square, piece_square))
+				if(connectedTo(board, square, piece_square, piece))
 					isAvalible = true;
 				if(piece_capture(board, square, piece_square, piece))
 					isAvalible = true;
 			}
 		}
 		
-		//sprawdza jeœli pola s¹ puste, jeœli s¹ zajête przez gracza o przeciwnym kol
-		// potrzebujê pionek oraz pole.... hmmm... ich po³o¿enia i wtedy bla bla bla
-		// co mam w bordzie: wszystkie pionki i pola i ich po³o¿enie??
-		
-		
 		if(isAvalible == true)
 			changeTurn(board);
 		return isAvalible;
 	}
 	
-	public static boolean connectedTo(Board board, Square square, Square piece_square){
+	public static boolean connectedTo(Board board, Square square, Square piece_square, Piece piece){
 		boolean connectedTo = false;
 		
 		if(piece_square != null){
-			if(square.getID()/(board.getSize()/2) % 2 == 0){
-				if(piece_square.getID() == square.getID() - 5 || piece_square.getID() == square.getID() - 4 || piece_square.getID() == square.getID() + 3 || piece_square.getID() == square.getID() + 4)
-					connectedTo = true;
+			
+			if(piece.getOwner() == OWNER.WHITE){
+				if(square.getID()/(board.getSize()/2) % 2 == 0){
+					if(piece_square.getID() == square.getID() - 5 || piece_square.getID() == square.getID() - 4)
+						connectedTo = true;
+				}else{
+					if(piece_square.getID() == square.getID() - 4 || piece_square.getID() == square.getID() - 3)
+						connectedTo = true;
+				}
 			}else{
-				if(piece_square.getID() == square.getID() - 4 || piece_square.getID() == square.getID() - 3 || piece_square.getID() == square.getID() + 4 || piece_square.getID() == square.getID() + 5)
-					connectedTo = true;
+				if(square.getID()/(board.getSize()/2) % 2 == 0){
+					if(piece_square.getID() == square.getID() + 3 || piece_square.getID() == square.getID() + 4)
+						connectedTo = true;
+				}else{
+					if(piece_square.getID() == square.getID() + 4 || piece_square.getID() == square.getID() + 5)
+						connectedTo = true;
+				}
 			}
+			
+//			if(square.getID()/(board.getSize()/2) % 2 == 0){
+//				if(piece_square.getID() == square.getID() - 5 || piece_square.getID() == square.getID() - 4 || piece_square.getID() == square.getID() + 3 || piece_square.getID() == square.getID() + 4)
+//					connectedTo = true;
+//			}else{
+//				if(piece_square.getID() == square.getID() - 4 || piece_square.getID() == square.getID() - 3 || piece_square.getID() == square.getID() + 4 || piece_square.getID() == square.getID() + 5)
+//					connectedTo = true;
+//			}
 		}
 		return connectedTo;
 	}
