5efca80bc735d5a09dfcdb836587a054a5d2e2ef
Sobczak <bartoszsobczak95@gmail.com>
2016-08-18 18:29:51 +0200

    Eliminate some bugs

--- src/Rules.java
+++ src/Rules.java
@@ -30,21 +30,19 @@ public abstract class Rules {
 	//square - square where you want to put piece, piece_square - square where the piece was before, piece - piece which you want to move
 	public static void move(Square square, Square piece_square ,Piece piece){
 		
-		if(board != null && piece != null && square != null && piece_square != null){
-			if(availablePiecesMove().contains(piece)){
+		if(board != null && piece != null && square != null && piece_square != null && availablePiecesMove().contains(piece)){
 				board.getSquares()[square.getID()].setPiece(piece);			//set piece at the new position
 				//if capture need to delete captured piece
-			}
-			else
+				
+				//promotion and change turn conditions
+				if(piece_saved == null){
+					if(achieveLastPool(square, piece))
+						piece.setType(PIECE_TYPE.KING);
+					changeTurn();
+				}	
+		}else
 				board.getSquares()[piece_square.getID()].setPiece(piece);	//set piece at the old position
-			
-			//promotion and change turn conditions
-			if(piece_saved == null){
-				if(achieveLastPool(square, piece))
-					piece.setType(PIECE_TYPE.KING);
-				changeTurn();
-			}	
-		}				
+						
 	}	
 	
 	//this method return list of pieces that can move at this moment
@@ -53,14 +51,14 @@ public abstract class Rules {
 		
 		availablePiecesMoveList = availablePiecesCapture();
 		if(availablePiecesMoveList.isEmpty()){
-			availablePiecesMoveList = availablePiecesMove();
+			availablePiecesMoveList = availablePiecesStep();
 			if(availablePiecesMoveList.isEmpty())
 				System.out.println("End of the game! Need to set end");
 		}else{
 			//if(!multiCapturePossible) nic, po prostu zwraca listę
-			if(multi_capture_possible){
+//			if(multi_capture_possible){
 				// ustala listę pionków o największej ilości bić i zwraca, lecz trzeba pamiętać że tutaj będzie inaczej bo będzie zapamiętywać pionek
-			}
+//			}
 		}
 		return availablePiecesMoveList;
 	}
